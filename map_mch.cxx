667f235d93c1a301e745991151d2d0074ea5c024
victorvalenciatorres <valencia@subatech.in2p3.fr>
2023-07-22 22:31:44 +0200

    Changing some names and comments

--- map_mch.cxx
+++ map_mch.cxx
@@ -55,6 +55,7 @@ std::pair<double, double> getTranslationOffset(int deId) {
         {200, {5.0, 0.0}}, {201, {-5.0, 0.0}}, {202, {-5.0, 10.0}}, {203, {5.0, 10.0}},
         {300, {5.0, 0.0}}, {301, {-5.0, 0.0}}, {302, {-5.0, 10.0}}, {303, {5.0, 10.0}},
         {400, {5.0, 0.0}}, {401, {-5.0, 0.0}}, {402, {-5.0, 10.0}}, {403, {5.0, 10.0}},
+
         //2nd type of Chamber Shape (chambers 5,6)
         {500, {5.0, 5.0}}, {501, {5.0, -10.0}}, {502, {5.0, -20.0}}, {503, {5.0, -30.0}},
         {504, {5.0, -40.0}}, {505, {0, -40.0}}, {506, {0, -30.0}}, {507, {0, -20.0}}, {508, {0, -10.0}},
@@ -79,34 +80,39 @@ std::pair<double, double> getTranslationOffset(int deId) {
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
-// Function to transform local contours to global contours coordinates 
+//  function to transform local contour coordinates to global contour coordinates
 std::vector<o2::mch::contour::Contour<double>> transformLocalToGlobal(int deId, bool bending, const o2::mch::geo::TransformationCreator& transformation) {
+
     CathodeSegmentation cSeg{deId, bending}; 
+
     std::vector<o2::mch::contour::Contour<double>> dualSampaContoursIn = getDualSampaContours(cSeg);
     std::vector<o2::mch::contour::Contour<double>> dualSampaContoursOut; 
+
     auto transformDeId = transformation(deId);
 
     for(int i = 0; i < dualSampaContoursIn.size(); i++) {
+
         auto dsContourIn = dualSampaContoursIn[i];
         o2::mch::contour::Contour<double> dsContourOut;
 
         for (auto p = 0; p < dsContourIn.size(); p++) {
-            auto polygIn = dsContourIn[p];
+
+            auto polygIn = dsContourIn[p];  // One contour may contains several polygons
             std::vector<o2::mch::contour::Vertex<double>> verticesOut;
 
             for (auto v = 0; v < polygIn.size(); v++) {
-                auto vertexIn = polygIn[v];
-                o2::math_utils::Point3D<float> lpos(vertexIn.x, vertexIn.y, 0.0); 
-                o2::math_utils::Point3D<float> gpos;        
 
-                // Transformations: Rotations + Translations (local --> global) 
-                transformDeId.LocalToMaster(lpos, gpos);
+                auto vertexIn = polygIn[v];  // Each polygon contains several vertex
+                o2::math_utils::Point3D<float> lpos(vertexIn.x, vertexIn.y, 0.0); // local
+                o2::math_utils::Point3D<float> gpos;  // global      
+
+                transformDeId.LocalToMaster(lpos, gpos);  // Transformations: Rotations + Translations (local --> global) with LocalToMaster
+
                 o2::mch::contour::Vertex<double> vertexOut;
 
                 auto offset = getTranslationOffset(deId);
                    
-            
-                    vertexOut.y = -gpos.Y();       // reflection anti-clockwise (minus value)
+                    vertexOut.y = -gpos.Y();       // Extra reflection to count the DeIds anticlockwise
                     vertexOut.y += offset.second;
                     vertexOut.x = gpos.X() + offset.first; 
              
@@ -121,27 +127,33 @@ std::vector<o2::mch::contour::Contour<double>> transformLocalToGlobal(int deId, 
         dualSampaContoursOut.push_back(dsContourOut);
     }
 
-
     return dualSampaContoursOut;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
-// GET ALL deID of a given Chamber
+// Getting all deID of a given Chamber
 std::vector<int> getAllDeIds(int nChamber) {
+
     std::vector<int> deIds;
-    int i_1 = *std::move(o2::mch::constants::deId2DeIndex(nChamber*100));       //Using move to convert optional integer into normal integer
+    int i_1 = *std::move(o2::mch::constants::deId2DeIndex(nChamber*100));     
     int i_2 = *std::move(o2::mch::constants::deId2DeIndex((nChamber+1)*100));
     int Chamberlength = i_2 -i_1;
 
     if(nChamber<10){
-        for (auto i = 0; i < Chamberlength; i++) {                              //Getting deIds for chambers from 1 - 9
+
+        // Obtaining DeIds for chambers from 1 to 9
+        for (auto i = 0; i < Chamberlength; i++) {                              
             deIds.push_back(o2::mch::constants::deIdsForAllMCH[i+i_1]);
         }
+
     }
     else{
+
+        // Obtaining DeIds for chamber 10
         for (int i = 0; i < 26; i++) {
-            deIds.push_back(o2::mch::constants::deIdsForAllMCH[i+i_1]);         //Getting deIds for chamber 10
+
+            deIds.push_back(o2::mch::constants::deIdsForAllMCH[i+i_1]);   
         }
     }
     return deIds;
@@ -149,58 +161,71 @@ std::vector<int> getAllDeIds(int nChamber) {
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
-// GET ALL DualSampa of a given deId
+// Getting DualSampas of a given deId
 std::vector<int> getDualSampas(int deId) {
+
     std::vector<int> dualSampas;
     const o2::mch::mapping::Segmentation& seg = o2::mch::mapping::segmentation(deId);
     seg.forEachDualSampa([&dualSampas](int ds) { dualSampas.push_back(ds); });
     return dualSampas;  
+
 }
 
 
-// GET ALL Bending or Non-Bending DualSampa of a given deId:
+//  Retrieve bending or non-bending DualSampas associated with a given DeId
 std::vector<int> getDualSampasBorNB(int deId, bool isBending) {
+
     std::vector<int> dualSampas;
     const o2::mch::mapping::Segmentation& seg = o2::mch::mapping::segmentation(deId);
+
     seg.forEachDualSampa([&dualSampas, isBending, &seg](int dualSampaId) {
+
         bool foundDualSampa = false;
+
         seg.forEachPad([&foundDualSampa, isBending, dualSampaId, &seg](int dePadIndex) {
             if (seg.isBendingPad(dePadIndex) == isBending && seg.padDualSampaId(dePadIndex) == dualSampaId) {
                 foundDualSampa = true;
                 return;  
             }
         });
+
         if (foundDualSampa) {
             dualSampas.push_back(dualSampaId);
         }
+
     });
     
-  
     return dualSampas; 
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
-// Get All Dual Sampas of n Chambers:
+// Getting all DualSampas of NChambers:
 std::vector<int> getAllDualSampas(int numChambers) {
+
     std::vector<int> allDualSampas;
     int count = 0; 
 
     for (int nChamber = 1; nChamber <= numChambers; nChamber++) {
+
         std::vector<int> deIds = getAllDeIds(nChamber);
         std::vector<int> dualSampas;
+
         for (auto deId : deIds) {
+
             std::vector<int> dsIds = getDualSampas(deId);
+
             for (auto dsId : dsIds) {
                 dsId += count; 
                 dualSampas.push_back(dsId);
             }
+
         }
+
         count = dualSampas.back() + 1; 
         allDualSampas.insert(allDualSampas.end(), dualSampas.begin(), dualSampas.end());
-    }
 
-    std::cout << std::endl;
+    }
 
     return allDualSampas;
 }
@@ -213,6 +238,7 @@ uint16_t convertDsIndextoDsId(o2::mch::DsIndex dsIndex)
     o2::mch::raw::DsDetId dsDetId = o2::mch::getDsDetId(dsIndex);
     uint16_t dsId = dsDetId.dsId();
     return dsId;
+
 }
 
 
@@ -221,25 +247,25 @@ uint16_t getDsIndexFromDsIdAndDeId(uint16_t dsId, uint16_t deId)
 {
   o2::mch::raw::DsDetId dsDetId(deId, dsId); // Create a DsDetId object with the given deId and dsId
   return o2::mch::getDsIndex(dsDetId); // Get the corresponding dsIndex from the getDsIndex function
+
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
-//Getting ClustersPerDualSampa TH1F Histogram stored in root file
-TH1F* getrootHistogram1() {
+//Getting ClustersPerDualSampa (TH1F Histogram stored in root file)
+TH1F* getrootHistogramLeft() {
 
     TFile* file = TFile::Open("/Users/valencia/test/ClustersMCH_LHC22t.root");
     TH1F* ClustersperDualSampa = (TH1F*)file->Get("ClustersPerDualSampa");
     return ClustersperDualSampa;
 }
 
-TH1F* getrootHistogram2() {
+TH1F* getrootHistogramRight() {
 
     TFile* file = TFile::Open("/Users/valencia/Desktop/Simulations/Clusters_Bending.root");
     TH1F* ClustersperDualSampa = (TH1F*)file->Get("Clusters");
     return ClustersperDualSampa;
 }
-
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
@@ -285,10 +311,13 @@ std::vector<std::string> colorGradiant()
    gStyle->SetPalette(kSunset);
    TColor::InvertPalette();
    TArrayI colors = TColor::GetPalette();
+
    for (int i = 0; i < colors.GetSize(); i++) {
+
     Int_t col = colors.At(i);
     TColor *tcol = gROOT->GetColor(col);
     hexcolors.push_back(tcol->AsHexString());
+
    }
 
     TColor::InvertPalette();     
@@ -297,24 +326,23 @@ std::vector<std::string> colorGradiant()
 
 }
 
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
-//Calculation of Nmax (maximum of clusters per chamber) <--> (maximum ratio of Clusters/DSArea per chamber)
+//Calculation of Nmax for two types of normalisation (maximum of clusters per chamber) <--> (maximum ratio of Clusters/DSArea per chamber)
 double calculateNmax(int nChamber, bool bending, const TH1F* ClustersperDualSampa, o2::mch::geo::TransformationCreator transformation, bool IsNormalizedPerDSArea) {
 
     // Load clusters from TH1F Histogram
     auto nClusters_dsindex = processClustersperDualSampa(ClustersperDualSampa);
     std::vector<int> nClusters = nClusters_dsindex.first;
-    std::vector<uint16_t> dsindex = nClusters_dsindex.second;
-
-    // Getting All DeIds for all Chambers
+    
+    // Getting all DeIds for all Chambers
     auto deIds = getAllDeIds(nChamber);
-    int dsIndex;
 
+    int dsIndex;
     double Nmax = 0.0;
 
     if (IsNormalizedPerDSArea == false) {
+
         // Calculate maximum number of clusters 
         for (auto deId : deIds) {
             auto dualSampaContoursOut = transformLocalToGlobal(deId, bending, transformation);
@@ -329,7 +357,9 @@ double calculateNmax(int nChamber, bool bending, const TH1F* ClustersperDualSamp
                 Nmax = std::max(Nmax, static_cast<double>(nClusters[dsIndex]));
             }
         }
+
     } else {
+
         // Calculate maximum ratio of clusters/DSArea
         double maxRatio = 0.0;
 
@@ -354,14 +384,15 @@ double calculateNmax(int nChamber, bool bending, const TH1F* ClustersperDualSamp
         }
 
         Nmax = maxRatio;
+
     }
 
     return Nmax;
 }
 
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
+//Gradient of Number for the color scale
 std::vector<double> numberGradient(double n, int m) {
     std::vector<double> grad;
     double step = n / (m - 1);
@@ -371,11 +402,11 @@ std::vector<double> numberGradient(double n, int m) {
     return grad;
 }
 
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//Add Rentangle with 255 colors + Numbers (from 0 to 1)
+//Add Rentangle with 255 colors + 11 Numbers (from 0 to Nmax)
 void addRectangleContour(int nChamber, o2::mch::contour::Contour<double>& contour, o2::mch::contour::SVGWriter& w, double Nmax, bool IsNormalizedPerDSArea) {
+
     double rectWidth;  
     double rectHeight;  
     double rectX;  
@@ -398,8 +429,6 @@ void addRectangleContour(int nChamber, o2::mch::contour::Contour<double>& contou
     title = "#/cm^2"; 
 
     };
-
-    
 
     //size of rectangle depending on chamber
     if (nChamber == 1 || nChamber == 2) {
@@ -458,6 +487,7 @@ void addRectangleContour(int nChamber, o2::mch::contour::Contour<double>& contou
     double stepY = rectHeight / 255.0;
 
     for (int i = 0; i < 255; i++) {
+
         double startY = rectY + i * stepY;
         double endY = rectY + (i + 1) * stepY;
 
@@ -470,20 +500,22 @@ void addRectangleContour(int nChamber, o2::mch::contour::Contour<double>& contou
             {endX, endY},
             {startX, endY}
         });
+
     }
 
     std::vector<double>numbers = numberGradient(Nmax,11);
-
-    //Produces Numbers 
     double n;
+    
+    // Produce numbers for the color scale
     for (int i = 10; i >= 0; i--) {
-        n = i * step; 
-        double textY = startY + (1 - i) * spacing; 
 
         std::stringstream ss;
+        n = i * step; 
+        double textY = startY + (1 - i) * spacing; 
         ss << std::fixed << std::setprecision(2) << numbers[i];
         std::string numberString = ss.str();
         w.text(numberString, textX, textY);
+
     }
 
 }
@@ -512,15 +544,17 @@ void svgChamber(o2::mch::contour::SVGWriter& w, int nChamber, bool bending, cons
     //Getting all deIds for all Chambers
     auto deIds = getAllDeIds(nChamber);
 
-    double epsilon = 1.e-6; //Small shift
+    double epsilon = 1.e-6; 
 
-    int colorId;
+    int colorId; 
+
+    double area = 0.0; 
 
     std::vector<std::pair<double, int>> areas; // Vector with Areas of DS Contours paired with dsIndex
 
-     double Nmax = calculateNmax( nChamber, bending, ClustersperDualSampa, transformation, IsNormalizedPerDSArea);  //Maximun Ratio
+    double Nmax = calculateNmax( nChamber, bending, ClustersperDualSampa, transformation, IsNormalizedPerDSArea);  //Maximun Ratio
     
-    // Contours of all deId transformated  + SVGWRITER of all dsContourOut bien
+    // All DeId transformated  + SVGWRITER of all chambers
     for (auto deId : deIds) {
         auto dualSampaContoursOut = transformLocalToGlobal(deId, bending, transformation);
         w.svgGroupStart("dualsampas");
@@ -528,21 +562,22 @@ void svgChamber(o2::mch::contour::SVGWriter& w, int nChamber, bool bending, cons
                 str += colors[0]; // Assign the first color in the vector
                 str += "; stroke-width: 0.25px; stroke: #333333;}";
                 w.addStyle(str);
-        // Get the map index to dsId
-        auto dsIds = getDualSampasBorNB(deId, bending); 
+        
+        auto dsIds = getDualSampasBorNB(deId, bending); // Corresponding all DSIndex to DsId
+
         for (auto i = 0; i < dualSampaContoursOut.size(); i++) {
 
-            auto& contour = dualSampaContoursOut[i]; // Get the current contour
+            auto& contour = dualSampaContoursOut[i]; 
 
-            double area = 0.0; // Area of a DS Contour
             for (const auto& poly : contour.getPolygons()) {
                 area += poly.signedArea();
             }
 
             auto dsId = dsIds[i];
-            int dsIndex = getDsIndexFromDsIdAndDeId(dsId, deId);
+            int dsIndex = getDsIndexFromDsIdAndDeId(dsId, deId); // Corresponding  DsId and DeId to DSIndex
             areas.push_back(std::make_pair(area, dsIndex));
 
+            // Select the type of Normalisation (Clusters/NClustersMax <--> Clusters/DSArea)
             if (IsNormalizedPerDSArea == true) {
                  colorId = int((nClusters[dsIndex] / std::abs(areas.back().first)) / (Nmax + epsilon) * colors.size());
             } else {
@@ -560,27 +595,25 @@ void svgChamber(o2::mch::contour::SVGWriter& w, int nChamber, bool bending, cons
         w.svgGroupEnd();
     }
 
-   
-    // Add rectangle for color scale + text:
+    // Add rectangle to display the color scale with numbers (0 to Nmax)
     o2::mch::contour::Contour<double> rectangleContour;
     addRectangleContour(nChamber, rectangleContour, w, Nmax, IsNormalizedPerDSArea);
 
     for(auto i =0; i<rectangleContour.size();i++){
 
-        w.polygon(rectangleContour[i],colors[rectangleContour.size()-1-i]);  //rectangle white to black
+        w.polygon(rectangleContour[i],colors[rectangleContour.size()-1-i]); 
 
     }
     
 
-  
 }
-
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 
 int main(int argc, char* argv[])
 {
 
+    // Generating various command arguments
     bool norm = false;
     bool green = false;
 
@@ -628,25 +661,26 @@ int main(int argc, char* argv[])
         {-450, -450, 450, 450}
     };
 
-    //Load Aligned Geometry from file.root
-    std::string name = "o2sim_geometry-aligned.root";
+    // Load  the aligned geometry from file.root
+    std::string alignedgeom = "o2sim_geometry-aligned.root";
 
-
-    // Create with SVGWriter the 10 Chambers with their respective bounding boxes
+    // Generate two sets (left and right) of 10 bending and non-bending chambers using SVGWriter
     for (auto isBendingPlane : {true, false}) {
+
         for (int i = 0; i < 10; i++) {
-            std::ofstream outv("CHAMBERS-" + std::to_string(i+1) + "-" +   //output file
-                            (isBendingPlane ? "B" : "NB") + ".html");
+
+            //output file
+            std::ofstream outv("CHAMBERS-" + std::to_string(i+1) + "-" + (isBendingPlane ? "B" : "NB") + ".html");
 
             // Creating bboxes 
             o2::mch::contour::SVGWriter wSegLeft(bboxes[i]);
             o2::mch::contour::SVGWriter wSegRight(bboxes[i]);
         
             // Creating Left and Right Chambers  
-            double maxRatioLeft = calculateNmax(i+1, isBendingPlane, getrootHistogram1(), loadGeometry(name), norm);
-            double maxRatioRight = calculateNmax(i+1, isBendingPlane, getrootHistogram2(), loadGeometry(name), norm);
-            svgChamber(wSegLeft, i+1, isBendingPlane, getrootHistogram1(), loadGeometry( name), maxRatioLeft, norm, green);
-            svgChamber(wSegRight, i+1, isBendingPlane, getrootHistogram2(), loadGeometry(name), maxRatioRight, norm, green);
+            double maxRatioLeft = calculateNmax(i+1, isBendingPlane, getrootHistogramLeft(), loadGeometry(alignedgeom), norm);
+            double maxRatioRight = calculateNmax(i+1, isBendingPlane, getrootHistogramRight(), loadGeometry(alignedgeom), norm);
+            svgChamber(wSegLeft, i+1, isBendingPlane, getrootHistogramLeft(), loadGeometry(alignedgeom), maxRatioLeft, norm, green);
+            svgChamber(wSegRight, i+1, isBendingPlane, getrootHistogramRight(), loadGeometry(alignedgeom), maxRatioRight, norm, green);
             
             // Write in HTML left and right chambers (using <div> tag)
             outv << "<div style='display:flex;justify-content:center'>" << std::endl;
@@ -655,10 +689,11 @@ int main(int argc, char* argv[])
             wSegRight.writeHTML(outv);
             outv << "</div>" << std::endl;
 
-        
         } 
+
     }
 
   return 0;
 
 }
+
